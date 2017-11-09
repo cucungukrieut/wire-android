da2b22f56a9c64bb6f32b55f467405ca75e6435c
JPonte <joao.ponte@wire.com>
2017-11-09 14:56:57 +0100

    Base scenes for create team account

--- app/src/main/scala/com/waz/zclient/WireApplication.scala
+++ app/src/main/scala/com/waz/zclient/WireApplication.scala
@@ -119,6 +119,7 @@ object WireApplication {
     bind [UserAccountsController]          to new UserAccountsController()
     bind [SharingController]         to new SharingController()
     bind [NavigationController]      to new NavigationController()
+    bind [CreateAccountController]   to new CreateAccountController()
 
 
     // current conversation data
